2ce16c87ca02d50cc3e98098ef019f848ae86ed8
Chechet Alexey <chechet.alexey@ya.ru>
2020-01-02 04:50:28 +0400

    form now is modal window

--- cosmos/js/script.js
+++ cosmos/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
     $('.space-slider').slick({
+      prevArrow: '<button class="prev-arrow arrow"></button>',
+      nextArrow: '<button class="next-arrow arrow"></button>'
     });
-  });
+});
+
+$('.open-popup').magnificPopup({
+  removalDelay: 300,
+  type:'inline',
+  midClick: true
+});
