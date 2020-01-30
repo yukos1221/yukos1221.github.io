20b8747232d0e8901c2863f2e22c80da89ee2bbb
Chechet Alexey <chechet.alexey@ya.ru>
2020-01-30 13:23:24 +0300

    removed some buttons

--- WDP/js/script.js
+++ WDP/js/script.js
@@ -24,4 +24,38 @@ $(document).ready(function(){
             // instead of a settings object
           ]
     });
-  });
+});
+
+const aboutEl = document.querySelector('#about-us-link');
+const servicingEl = document.querySelector('#servicing-link');
+const portfolioEl = document.querySelector('#portfolio-link');
+const blogEl = document.querySelector('#blog-link');
+const contactEl = document.querySelector('#contact-us-link');
+const aboutEl2 = document.querySelector('#about-us-link2');
+const servicingEl2 = document.querySelector('#servicing-link2');
+
+const aboutus = (e) => {
+  e.preventDefault();
+  document.querySelector('#about-us-section').scrollIntoView();
+}
+const servicing = (e) => {
+  e.preventDefault();
+  document.querySelector('#servicing-section').scrollIntoView();
+}
+aboutEl.onclick = aboutus;
+servicingEl.onclick = servicing;
+aboutEl2.onclick = aboutus;
+servicingEl2.onclick = servicing;
+
+portfolioEl.onclick = function(e){
+  e.preventDefault();
+  document.querySelector('#portfolio-section').scrollIntoView();
+}
+blogEl.onclick = function(e){
+  e.preventDefault();
+  document.querySelector('#blog-section').scrollIntoView();
+}
+contactEl.onclick = function(e){
+  e.preventDefault();
+  document.querySelector('#contact-us-section').scrollIntoView();
+}
