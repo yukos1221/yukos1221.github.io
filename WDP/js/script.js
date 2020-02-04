0ced7b01a983b8c7b042ceecdad786c1ac7cfcfe
Chechet Alexey <chechet.alexey@ya.ru>
2020-02-04 23:26:25 +0300

    added modal servicing window and fixed member images

--- WDP/js/script.js
+++ WDP/js/script.js
@@ -1,61 +1,122 @@
-$(document).ready(function(){
-    $('.our-team-slider').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 500,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
+const aboutEl = document.querySelectorAll('.about-us-link');
+const servicingEl = document.querySelectorAll('.servicing-link');
+const portfolioEl = document.querySelectorAll('.portfolio-link');
+const blogEl = document.querySelectorAll('.blog-link');
+const contactEl = document.querySelectorAll('.contact-us-link');
+const servicingButtons = document.querySelectorAll('.ourservbtn');
+const servicingPage = document.querySelector('.servicing-modal');
+const modalImgElem = document.querySelector('#modal-img')
+const modalTitleElem = document.querySelector('#modal-title')
+
+aboutEl.forEach((item) => {
+  item.addEventListener('click', (item) => {
+    item.preventDefault();
+    document.querySelector('#about-us-section').scrollIntoView();
+  })
+})
+servicingEl.forEach((item) => {
+  item.addEventListener('click', (item) => {
+    item.preventDefault();
+    document.querySelector('#servicing-section').scrollIntoView();
+  })
+})
+portfolioEl.forEach((item) => {
+  item.addEventListener('click', (item) => {
+    item.preventDefault();
+    document.querySelector('#portfolio-section').scrollIntoView();
+  })
+})
+blogEl.forEach((item) => {
+  item.addEventListener('click', (item) => {
+    item.preventDefault();
+    document.querySelector('#blog-section').scrollIntoView();
+  })
+})
+contactEl.forEach((item) => {
+  item.addEventListener('click', (item) => {
+    item.preventDefault();
+    document.querySelector('#contact-us-section').scrollIntoView();
+  })
+})
+
+const closeServicingPage = (e) => {
+  const target = e.target.classList;
+  if (target == 'servicing-modal' || 
+    target =='exitbtn' ||
+    target == 'far fa-times-circle') {
+      modalImgElem.classList.remove('fa-search', 'fa-folder-open',
+        'fa-headset', 'fa-book', 'fa-lightbulb', 'fa-palette');
+      servicingPage.style.display = "none";
+    }
+}
+
+const showServicingPage = (e) => {
+  const name = e.target.name;
+  let title;
+  let symbol;
+  switch (name) {
+    case 'btn1':
+      title = 'Research';
+      symbol = 'fa-search';
+      break;
+    case 'btn2':
+      title = 'Portfolio';
+      symbol = 'fa-folder-open';
+      break;
+    case 'btn3':
+      title = 'Support';
+      symbol = 'fa-headset';
+      break;
+    case 'btn4':
+      title = 'Documentation';
+      symbol = 'fa-book';
+      break;
+    case 'btn5':
+      title = 'Development';
+      symbol = 'fa-lightbulb';
+      break;
+    case 'btn6':
+      title = 'Design';
+      symbol = 'fa-palette';
+      break;
+  }
+  modalImgElem.classList.add(symbol);
+  modalTitleElem.textContent = title;
+
+  servicingPage.style.display = 'flex';
+}
+
+servicingButtons.forEach( item => {
+  item.addEventListener('click', showServicingPage);
 });
+servicingPage.addEventListener('click', closeServicingPage);
 
-const aboutEl = document.querySelector('#about-us-link');
-const servicingEl = document.querySelector('#servicing-link');
-const portfolioEl = document.querySelector('#portfolio-link');
-const blogEl = document.querySelector('#blog-link');
-const contactEl = document.querySelector('#contact-us-link');
-const aboutEl2 = document.querySelector('#about-us-link2');
-const servicingEl2 = document.querySelector('#servicing-link2');
-
-const aboutus = (e) => {
-  e.preventDefault();
-  document.querySelector('#about-us-section').scrollIntoView();
-}
-const servicing = (e) => {
-  e.preventDefault();
-  document.querySelector('#servicing-section').scrollIntoView();
-}
-aboutEl.onclick = aboutus;
-servicingEl.onclick = servicing;
-aboutEl2.onclick = aboutus;
-servicingEl2.onclick = servicing;
-
-portfolioEl.onclick = function(e){
-  e.preventDefault();
-  document.querySelector('#portfolio-section').scrollIntoView();
-}
-blogEl.onclick = function(e){
-  e.preventDefault();
-  document.querySelector('#blog-section').scrollIntoView();
-}
-contactEl.onclick = function(e){
-  e.preventDefault();
-  document.querySelector('#contact-us-section').scrollIntoView();
-}
+$(document).ready(function(){
+  $('.our-team-slider').slick({
+      prevArrow: '<button class="prev-arrow arrow"></button>',
+      nextArrow: '<button class="next-arrow arrow"></button>',
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      dots: true,
+      responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+  });
+});
